16f893af7b78681947e2b76c6f84f6dde7c5f612
sehej3 <116433502+sehej3@users.noreply.github.com>
2022-10-22 15:42:54 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,34 @@
+# for i in range(4):
+#     for j in range(4):
+#         print("*", end="" )
+#     print("")
+
+
+# for i in range(4):
+#     for j in range(i+1):
+#         print("*", end="" )
+#     print("")
+
+
+# k=4
+# for i in range(4):
+#     for j in range(k):
+#         print("*", end="" )
+#     print("")
+    
+#     k=k-1
+
+
+k=3
+for i in range(4):
+    for j in range(k):
+        print(" ", end="" )
+
+
+    k=k-1
+
+
+
+    for u in range(i+1):
+        print("* ", end="" )
+    print("")
